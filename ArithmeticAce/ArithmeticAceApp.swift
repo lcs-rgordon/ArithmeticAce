ea06e02dfa02a78aa2d76ae1af57eaf0bd3f884e
Russell Gordon <github@russellgordon.ca>
2022-02-09 07:26:15 -0500

    Remove tab view

--- ArithmeticAce/ArithmeticAceApp.swift
+++ ArithmeticAce/ArithmeticAceApp.swift
@@ -11,32 +11,7 @@ import SwiftUI
 struct ArithmeticAceApp: App {
     var body: some Scene {
         WindowGroup {
-            TabView {
-                AdditionView()
-                    .tabItem {
-                        Image(systemName: "plus.circle")
-                        Text("Add")
-                    }
-
-                SubtractionView()
-                    .tabItem {
-                        Image(systemName: "minus.circle")
-                        Text("Subtract")
-                    }
-
-                MultiplicationView()
-                    .tabItem {
-                        Image(systemName: "multiply.circle")
-                        Text("Mutiply")
-                    }
-
-                DivisionView()
-                    .tabItem {
-                        Image(systemName: "divide.circle")
-                        Text("Divide")
-                    }
-
-            }
+            AdditionView()
         }
     }
 }
